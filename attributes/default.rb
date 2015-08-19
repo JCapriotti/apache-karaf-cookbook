a6a33fef6154657d5409f6d167fb4288c2eaf196
Jason Capriotti <jcapriotti@directs.com>
2015-08-19 15:05:28 -0500

    Working cookbook with karaf feature support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
 default['karaf']['version'] = '3.0.3'
 default['karaf']['url'] = ''
 default['karaf']['install_path'] = '/usr/local'
+default['karaf']['install_java'] = false
+
+default['karaf']['feature_repos'] = {'hawtio' => '1.4.51'}
+default['karaf']['features'] = ['hawtio']
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,18 +3,68 @@
 # Recipe:: default
 #
 
-include_recipe "java"
-include_recipe "ark"
+install_path = node['karaf']['install_path']
+karaf_path = "#{install_path}/karaf"
+start_command = "#{karaf_path}/bin/start"
+client_command = "#{karaf_path}/bin/client"
 
+if node['karaf']['install_java']
+  include_recipe 'java'
+end
 
 if node['karaf']['url'].empty?
-  karaf_url = "http://www.apache.org/dyn/closer.cgi/karaf/#{node['karaf']['version']}/apache-karaf-#{node['karaf']['version']}.tar.gz"
+  karaf_url = "http://archive.apache.org/dist/karaf/#{node['karaf']['version']}/apache-karaf-#{node['karaf']['version']}.tar.gz"
 else
   karaf_url = node['karaf']['url']
 end
 
 ark "karaf" do
   url karaf_url
-  path node['karaf']['install_path']
+  path install_path
   action :put
 end
+
+execute 'start karaf' do
+  command <<-EOF
+    #{start_command}
+    sleep 5
+  EOF
+end
+
+
+execute 'install karaf service wrapper' do
+  command <<-EOF
+    #{client_command} -u karaf feature:install service-wrapper
+    #{client_command} -u karaf wrapper:install
+  EOF
+  not_if  { ::File.exists?("#{karaf_path}/bin/karaf-service") }
+end
+
+link '/etc/init.d/karaf-service' do
+  to "#{karaf_path}/bin/karaf-service"
+  link_type :symbolic
+end
+
+service 'karaf-service' do
+  supports :status => true, :restart => true
+  action :start
+end
+
+# Install feature repos
+node['karaf']['feature_repos'].each_pair do |name, version|
+  execute 'install feature repo' do
+    command <<-EOF
+      #{client_command} -u karaf feature:repo-add #{name} #{version}
+    EOF
+  end
+end
+
+# Install features
+node['karaf']['features'].each do |name|
+  execute 'install feature' do
+    command <<-EOF
+      #{client_command} -u karaf feature:install #{name}
+    EOF
+  end
+end
+
