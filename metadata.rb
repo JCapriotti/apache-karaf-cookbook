cf4598c019967eea77f9ef69d3997551e2e8dff8
Jason Capriotti <jcapriotti@directs.com>
2016-01-25 11:19:17 -0600

    Tweak to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'jason.capriotti@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures karaf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/jcapriotti/apache-karaf-cookbook'
 version          '0.2.0'
 
 depends 'ark'
