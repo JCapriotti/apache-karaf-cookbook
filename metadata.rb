cb4a9248badf54a516187055e72f712e1c538ccd
Jason Capriotti <jcapriotti@directs.com>
2016-01-27 08:39:12 -0600

    Add supports metadata

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures karaf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/jcapriotti/apache-karaf-cookbook'
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'ark'
 depends 'java'
+
+supports 'centos'
+supports 'fedora'
