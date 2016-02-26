b3e830161b4ee7139b27d8c68b7c0925bfba595a
Jason Capriotti <jcapriotti@directs.com>
2016-02-25 19:45:08 -0600

    Fix rubocop

--- test/integration/slow-cloud/serverspec/karaf_spec.rb
+++ test/integration/slow-cloud/serverspec/karaf_spec.rb
@@ -4,6 +4,6 @@ describe 'slow-cloud' do
   it_behaves_like 'karaf_install'
 
   describe file('/tmp/karaf-install.log') do
-    its(:content) { should match /Command not found: feature:install/ }
+    its(:content) { should match(/Command not found: feature:install/) }
   end
 end
