a873b9787c8daad68f8f6fb9da8f6daec3c97cc6
Alex Riedl <ariedl@directs.com>
2016-02-29 15:37:44 -0600

    Added resource to install karaf bundles

---
+++ resources/karaf_bundle.rb
@@ -0,0 +1,34 @@
+resource_name :karaf_bundle
+
+property :install_path, kind_of: String, default: '/usr/local'
+property :client_user, kind_of: String, default: 'karaf'
+property :bundle_name, kind_of: String, required: true, name_property: true
+property :version, kind_of: String, default: ''
+property :wrap, kind_of: [TrueClass, FalseClass], default: false
+
+default_action :install
+
+client_command = 'bin/client'
+def karaf_path
+  "#{install_path}/karaf"
+end 
+
+action :install do
+  if version.empty?
+    bundle_string = bundle_name
+  else
+    bundle_string = "#{bundle_name}/#{version}"
+  end
+
+  bundle_string = "mvn:#{bundle_string}"
+  if wrap?
+      bundle_string = "wrap:#{bundle_string}"
+  end
+
+  bash 'install bundle' do
+    cwd  karaf_path
+    code <<-EOH
+      #{client_command} -r 20 -d 3 -u karaf bundle:install -s #{bundle_string}
+    EOH
+  end
+end
